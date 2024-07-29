b7c9c6377cf514628c484156a6c48ac1c18351fa
raguraman <raguraman.mps6066@gmail.com>
2024-07-29 21:29:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+import { StyleSheet, Text, View } from "react-native";
+
+export default App = () => {
+  const thankYou = "thank you for using";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi, how are youu!</Text>
+      <Text>bye.. {thankYou}</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hi, how are youu! class component</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+export default App;
+*/
