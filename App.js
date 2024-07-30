6c49ed7431492d629b689d42759acec81a1b078c
raguraman <raguraman.mps6066@gmail.com>
2024-07-30 15:46:44 +0530

    list and styles

--- App.js
+++ App.js
@@ -1,89 +1,40 @@
-import React, { useState } from "react";
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import React from "react";
+import { SectionList, Text, View } from "react-native";
+import styles from "./commonStyle";
 
-export default App = () => {
-  const [people, setpeople] = useState([
-    { day: "sunday", key: "1" },
-    { day: "monday", key: "2" },
-    { day: "tuesday", key: "3" },
-    { day: "wednesday", key: "4" },
-    { day: "thusday", key: "5" },
-    { day: "friday", key: "6" },
-    { day: "saturday", key: "7" },
-    { day: "sunday", key: "11" },
-    { day: "monday", key: "12" },
-    { day: "tuesday", key: "13" },
-    { day: "wednesday", key: "14" },
-    { day: "thusday", key: "15" },
-    { day: "friday", key: "16" },
-    { day: "saturday", key: "17" },
-  ]);
+const App = () => {
+  const DATA = [
+    {
+      title: "Main Dishes",
+      data: ["Pizza", "Burger", "Risotto"],
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"],
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"],
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      {
-        /* <ScrollView>
-        {people.map((item) => {
-          <View key={item.key}>
-            <Text style={styles.text}>{item.day}</Text>
-          </View>;
-        })}
-      </ScrollView> */
-
-        <FlatList
-          numColumns={2}
-          data={people}
-          // keyExtractor={item.id} if id instead of key
-          renderItem={({ item }) => <Text style={styles.text}>{item.day}</Text>}
-        />
-      }
+      <Text style={[styles.header, styles.extra]}>Sample text</Text>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    backgroundColor: "blue",
-    padding: 20,
-  },
-  text: {
-    backgroundColor: "orange",
-    padding: 20,
-    fontWeight: "bold",
-    margin: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 10,
-    margin: 10,
-    width: 250,
-  },
-});
-
-/*
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>hi, how are youu! class component</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-}
-
 export default App;
-*/
---
+++ commonStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: "#fff",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  header: {
+    fontSize: 24,
+    backgroundColor: "#f4f4f4",
+    padding: 5,
+  },
+  extra: {
+    color: "red",
+  },
+});
+export default styles;
