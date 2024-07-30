ec394c5a86f66391ae9c4869608f802fdb0b9302
raguraman <raguraman.mps6066@gmail.com>
2024-07-30 10:40:44 +0530

    styles props

--- App.js
+++ App.js
@@ -2,23 +2,60 @@ import { StatusBar } from "expo-status-bar";
 
 import { StyleSheet, Text, View } from "react-native";
 
+const Header = (props) => {
+  return (
+    <View>
+      <Text>Hii Good morning {props.name}</Text>
+    </View>
+  );
+};
+
+const Body = () => {
+  return (
+    <View>
+      <Text>This is sample body content</Text>
+    </View>
+  );
+};
+
+const Footer = () => {
+  return (
+    <View>
+      <Text>Copy rights of company</Text>
+    </View>
+  );
+};
+
 export default App = () => {
-  const thankYou = "thank you for using";
   return (
     <View style={styles.container}>
-      <Text>hi, how are youu!</Text>
-      <Text>bye.. {thankYou}</Text>
-      <StatusBar style="auto" />
+      <Header name="ragu" />
+      <Header name="anbu" />
+      <Header name="kumar" />
+      <Body />
+      <Footer />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    height: 200,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+
+    backgroundColor: "yellow",
+  },
+  header: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  text: {
+    backgroundColor: "orange",
+    padding: 20,
+    fontWeight: "bold",
   },
 });
 
